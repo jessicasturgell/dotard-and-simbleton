0ade50758d385bfd2b1c2a53a956333bf38065c7
Jessica Sturgell <sturgell01@gmail.com>
2024-05-13 14:40:37 -0400

    added search bar functionality

---
+++ scripts/searchBar.js
@@ -0,0 +1,31 @@
+import { getBusinesses } from "./database.js";
+const businesses = getBusinesses()
+
+export const searchBar = (outEl) => {
+    document
+    .querySelector("#companySearch")
+    .addEventListener("keypress", keyPressEvent => {
+        if (keyPressEvent.charCode === 13) {
+            /* WHEN  USER PRESSES ENTER, FIND MATCHING BUSINESS */
+            const foundBusiness = businesses.find(
+                business =>
+                    business.companyName.includes(keyPressEvent.target.value)
+            );
+
+            outEl.innerHTML = `
+                <h2>
+                ${foundBusiness.companyName}
+                </h2>
+                <section>
+                ${foundBusiness.addressFullStreet}
+
+                </section>
+                <section>
+                ${foundBusiness.addressCity},
+                ${foundBusiness.addressStateCode}
+                ${foundBusiness.addressZipCode}
+                </section>
+            `;
+        }
+    });
+}
