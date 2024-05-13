9338ddefe784cbf9c761abb35492846b48af4b42
Jessica Sturgell <sturgell01@gmail.com>
2024-05-13 14:42:14 -0400

    added imports and innerHTML

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,18 @@
+import { businessesArray } from "./businesses.js"
 import { getBusinesses } from "./database.js"
-const businesses = getBusinesses()
+import { manufacturingArray } from "./manufacturingBusinesses.js"
+import { newYorkArray } from "./newYorkBusinesses.js"
+import { agentsArray } from "./purchasingAgents.js"
+import { searchBar } from "./searchBar.js"
 
 const outEl = document.querySelector("#output")
+const businesses = getBusinesses()
 
-let html = `
-            <div class ="header">
-                <h1>Dotard & Simbleton</h1>
-            </div>
-            <h1>Active Businesses</h1>
-            `
+// initialize search bar functionality
+searchBar(outEl)
 
-businesses.forEach(business => {
-    html += `
-            <h2>${business.companyName}</h2>
-                <section>
-                    ${business.addressFullStreet}
-                </section>
-                <section>
-                    ${business.addressCity}, ${business['addressStateCode']}, ${business['addressZipCode']}
-                </section>
-                `
-    html += "<hr/>"
-});
+// generate html
+const html = businessesArray() + newYorkArray() + manufacturingArray() + agentsArray()
 
+// render html
 outEl.innerHTML = html
